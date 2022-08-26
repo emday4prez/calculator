7a47b9dfbb4f84f40d04f9a283c95eb90e432dd7
Emerson <emerson.day@gmail.com>
2022-08-25 23:07:19 -0400

    struggled with js functionality

--- script.js
+++ script.js
@@ -1,28 +1,97 @@
-function add(n1,n2){
- return n1 + n2
+ class Calculator{
+  constructor(previousOperandTextElement, currentOperandTextElement){
+   this.previousOperandTextElement = previousOperandTextElement
+   this.currentOperandTextElement = currentOperandTextElement
+   this.clear()
+  }
+  clear(){
+   this.currentOperand = ''
+   this.previousOperand = ''
+   this.operation = undefined
+  }
+  delete(){
+
+  }
+  appendNumber(number){
+   if(number === '.' && this.currentOperand.includes('.')) return
+   this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+  chooseOperation(operation){
+   if(this.currentOperand === '') return
+   if(this.previousOperand !== ''){
+    this.compute()
+   }
+   this.operation = operation 
+   this.previousOperand = this.currentOperand
+   this.currentOperand = ''
+  }
+  compute(){
+
+  }
+  updateDisplay(){
+   this.currentOperandTextElement.innerText = this.currentOperand
+   this.previousOperandTextElement.innerText = this.previousOperand
+  }
+ }
+ 
+ 
+ const numberButtons = document.querySelectorAll('[data-number]')
+ const operationButtons = document.querySelectorAll('[data-operation]')
+ const equalsButton = document.querySelector('[data-equals]')
+ const deleteButton = document.querySelector('[data-delete]')
+ const allClearButton = document.querySelector('[data-all-clear]')
+ const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+ const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button => {
+ button.addEventListener('click', () => {
+  calculator.appendNumber(button.innerText)
+  calculator.updateDisplay()
+ })
+})
+
+operationButtons.forEach(button => {
+ button.addEventListener('click', () => {
+  calculator.chooseOperation(button.innerText)
+  calculator.updateDisplay()
+ })
+})
+
+
+
+
+function add(n1, n2) {
+    return n1 + n2
 }
-function subtract(n1,n2){
- return n1 - n2
+function subtract(n1, n2) {
+    return n1 - n2
 }
-function multiply(n1,n2){
- return n1 * n2
+function multiply(n1, n2) {
+    return n1 * n2
 }
-function divide(n1,n2){
- return n1 / n2
+function divide(n1, n2) {
+    return n1 / n2
 }
 
-function operate(operator, a, b){
- switch(operator){
-  case '+':
-   return add(a, b)
-  case '-':
-   return subtract(a, b)
-  case '*':
-   return multiply(a,b);
-  case '/':
-   if(b === 0) return null;
-   return divide(a, b)
-  default:
-   return null;
- }
+function clear() {
+    digits.textContent = ''
+    firstArg = ''
+    secondArg = ''
+}
+function operate(op, a, b) {
+    switch (op) {
+        case '+':
+            return add(a, b)
+        case '-':
+            return subtract(a, b)
+        case '*':
+            return multiply(a, b)
+        case '/':
+            if (b === 0) return null
+            return divide(a, b)
+        default:
+            return null
+    }
 }
